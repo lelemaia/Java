cac32b2fcae70cbdfc5234b4a3de1c112372ae33
lelemaia <rm94222@fiap.com.br>
2022-12-26 20:13:42 -0300

    .

---
+++ javapoo/src/TDD/CalculadoraTestes.java
@@ -0,0 +1,15 @@
+package TDD;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraTestes {
+    @Test
+    public void deveriaSomarDoisNumerosPositivos() {
+
+        Calculadora calc1 = new Calculadora();
+        int soma = calc1.somar(3, 7);
+
+        Assert.assertEquals(10, soma);
+    }
+}
